6d2133b00fbddddedc6bfabf80ed99b1e3ad96e4
XiangyuG <xg673@nyu.edu>
2022-04-18 13:24:26 -0400

    remove old preprocessed files

--- Domino_mutations_preprocessed_2/rcp/stats.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DOMINO=/home/ruijief/CaT-Preprocessor/
-for p in `ls | grep "\.c"`; do
-	time $DOMINO/domino $p > /dev/null
-done
--- Domino_mutations_preprocessed_2/run-domino.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-rm -rf *.domino
-for x in `ls $1 | grep .c` ; do 
-  time  $DOMINO/domino $1/$x > $x.in
-done
-
